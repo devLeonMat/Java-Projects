88b1400c8584a2b875cc9e48d09db80354f34335
Leon Matias Roslin <leonmatias1991@gmail.com>
2019-08-07 06:26:37 -0500

    change

--- Spring-Framework/spring-boot-jboss/target/classes/import.sql
+++ Spring-Framework/spring-boot-jboss/target/classes/import.sql
@@ -1,5 +1,4 @@
 /* Populate tables */
-INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-01', '');
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('John', 'Doe', 'john.doe@gmail.com', '2017-08-02', '');
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2017-08-03', '');
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Jane', 'Doe', 'jane.doe@gmail.com', '2017-08-04', '');
