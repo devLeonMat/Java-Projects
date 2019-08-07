a8dbf6dd30b995bff8febf6c90d7e533fae39c32
Leon Matias Roslin <leonmatias1991@gmail.com>
2019-08-06 21:41:54 -0500

    add ws

--- Web Services/EWS060819ApiRestCrud/src/main/java/com/rleon/EWS060819ApiRestCrud/controller/UserController.java
+++ Web Services/EWS060819ApiRestCrud/src/main/java/com/rleon/EWS060819ApiRestCrud/controller/UserController.java
@@ -1,5 +1,35 @@
 package com.rleon.EWS060819ApiRestCrud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rleon.EWS060819ApiRestCrud.entity.User;
+import com.rleon.EWS060819ApiRestCrud.repository.UserRepository;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping("/getUsers")
+	public Iterable<User> getUsers(){
+		return repository.findAll();
+	}
+	
+	@PostMapping("/add/{name}/{lastname}")
+	public void addUser(@PathVariable("name") String name, @PathVariable("lastname") String lastname) {
+		
+		User u = new User();
+		u.setName(name);
+		u.setLastName(lastname);
+		repository.save(u);
+		
+	}
 
 }
