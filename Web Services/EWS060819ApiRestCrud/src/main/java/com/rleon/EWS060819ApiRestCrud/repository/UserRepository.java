ad89f176498cc489fbdbafca50ead62568eb7dea
Leon Matias Roslin <leonmatias1991@gmail.com>
2019-09-09 23:00:48 -0500

    add canges

--- Web Services/EWS060819ApiRestCrud/src/main/java/com/rleon/EWS060819ApiRestCrud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rleon.EWS060819ApiRestCrud.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rleon.EWS060819ApiRestCrud.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-
-}
--- Web Services/EWS060819ApiRestCrud/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into User(name, lastname ) values ('roslin','leon matias');
-insert into User(name, lastname ) values ('Jose','menacho');
-insert into User(name, lastname ) values ('giovana','loja fernandez');
